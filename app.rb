f09dff5f96b0612e18a361f8468707442cb5f4a4
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-03 23:06:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @piglatinized = PigLatinizer.new(params[:user_phrase]).piglatinize
+    @piglatinized = PigLatinizer.new(params[:user_phrase]).to_pig_latin
 
     erb :piglatinize
   end
