0af34f249dae34e60430b565c543c72e36f0dad3
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-03 22:41:09 +0000

    automatically backed up by learn

---
+++ spec/models/piglatinize.rb
@@ -0,0 +1,33 @@
+class PigLatinizer
+  attr_reader :phrase
+  VOWELS = ["a", "e", "i", "o", "u"]
+
+  def initialize(phrase)
+    @phrase = phrase
+    self.piglatinize
+  end
+
+  def words
+    @phrase.split
+  end
+
+  def starts_with_vowel(word)
+    VOWELS.include?(word[0])
+  end
+
+  def piglatinize_word(word)
+    if starts_with_vowel(word)
+      piglatinized = word + 'hay'
+    else
+      first_vowel = word.index(/[aeiou]/)
+      move_letters = word[0..(first_vowel - 1)]
+      piglatinized = word[first_vowel..-1] + move_letters + 'ay'
+    end
+    piglatinized
+  end
+
+  def piglatinize(phrase)
+    piglatinized = phrase.words.each {|word| piglatinize_word(word)}
+  end
+
+end
